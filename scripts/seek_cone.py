62b7e558391d47fdb6cd7c84c3d58f94dd145bb8
binary-bard <ajg.public@yahoo.com>
2017-02-21 17:06:48 -0800

    Fix steering step size since angle is in radians and not degrees

--- scripts/seek_cone.py
+++ scripts/seek_cone.py
@@ -12,29 +12,6 @@ from cone_finder.msg import location_msgs as Locations
 msg = """
 cone_seeker: reading cone_finder location messages and 
              publishing OverrideRCIn messages
-------------------------------------------------------
-Throttle:
-    e    increase 5%
-    d    set to zero
-    c    decrease 5%
-    w    increase max throttle 5%
-    x    decrease max throttle 5%
-    q    increase min throttle 5%
-    z    decrease min throttle 5%
-
-Steering:
-    j    increase servo L 5%
-    k    set steering to neutral
-    l    increase servo R 5%
-    u    increase max L servo 5%
-    m    decrease max L servo 5%
-    o    increase max R servo 5%
-    .    decrease max R servo 5%
-
-any other key sets throttle and steering neutral
-
-CTRL-C to quit
-------------------------------------------------------------
 """
 
 #settings = termios.tcgetattr(sys.stdin)
@@ -45,8 +22,8 @@ throttle_limits = [1200, 1500, 1800]  # middle is neutral
 steering = steering_limits[1]  # start neutral
 throttle = throttle_limits[1]  # start neutral
 
-# We will get angle between +90 to -90 for steering
-STEER_SIZE = 2   # normalized step size for adjustments for steering
+# We will get angle between +pi/2 to -pi/2 for steering
+STEER_SIZE = 60   # normalized step size for adjustments for steering
 STEP_SIZE  = 0.5 # normalized step size for adjustments for throttle
 
 def seek_cone(loc):
@@ -54,6 +31,7 @@ def seek_cone(loc):
     poses = sorted(loc.poses, key=lambda loc: loc.y)
     cone_loc = poses[0]
     # If cone is to the right
+    print("Theta = %f" % cone_loc.theta)
     steering = steering_limits[1] + STEER_SIZE*cone_loc.theta
     y = cone_loc.y - 80
     throttle = throttle_limits[1]
